173661519f02415b630e14ba38529a4c02e8aebf
Gabriela <severogabriela4@gmail.com>
2025-05-20 09:36:51 -0300

    fazendo conexão, fazer alterações.

---
+++ data/conexao.py
@@ -0,0 +1,21 @@
+import mysql.connector
+
+class Conexao:
+    
+    def criar_conexao():
+        # Criando a conexão com o banco de dados
+        # conexao = mysql.connector.connect(host = "bdgodofredo-alexstocco-93db.b.aivencloud.com",
+        #                                 port = 27974,
+        #                                 user = "3ds",
+        #                                 password = "banana",
+        #                                 database = "db_feedback")
+        
+        
+        # Criando a conexão com o banco de dados
+        conexao = mysql.connector.connect(host = "localhost",
+                                        port = 3306,
+                                        user = "3ds",
+                                        password = "banana",
+                                        database = "db_feedback")
+        
+        return conexao
