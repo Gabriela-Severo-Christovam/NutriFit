5c993dd97805fea332aca603be4f83a5d35beb65
Gabriela <severogabriela4@gmail.com>
2025-05-27 10:11:35 -0300

    Criando control para produtos

---
+++ model/control_produto.py
@@ -0,0 +1,37 @@
+from data.conexao import Conexao
+
+class Produtos:
+    def recuperar_produtos():
+        #Criar conexão
+        conexao = Conexao.criar_conexao()
+
+        # O cursor será responsável por manipular
+        cursor = conexao.cursor(dictionary = True)
+
+        # Criando o sql que será executado
+        sql = """
+                SELECT
+                    tbProdutos.nome AS nome_produto,
+                    tbProdutos.descricao AS descricao_produto,
+                    tbProdutos.preco,
+                    tbCategoria.descricao AS categoria_produto,
+                    tbFotosProdutos.url AS url_foto
+                FROM
+                    tbProdutos
+                INNER JOIN
+                    tbCategoria
+                    ON tbProdutos.cod_categorias = tbCategoria.cod_categorias
+                INNER JOIN
+                    tbFotosProdutos
+                    ON tbProdutos.cod_produto = tbFotosProdutos.cod_produto;"""
+
+        #Executando o comando sql
+        cursor.execute(sql)        
+
+        #Recuperando os dados e jogando em uma varialvel
+        resultado = cursor.fetchall()
+
+        #Fecho a conexão (como não ouve alteração não preciso do commit)
+        conexao.close()
+
+        return resultado
